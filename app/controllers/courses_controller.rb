816cc53dd9cb38cc5dbbf1945d207e62020777a4
Gaston Roxas <gacaro51597@gmail.com>
2022-03-01 11:05:41 +0100

    added index and show to course controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,24 @@
 class CoursesController < ApplicationController
+  def index
+    @courses = Course.all
+  end
+
+  def show
+    @course = Course.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
