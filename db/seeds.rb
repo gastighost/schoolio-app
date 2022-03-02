fe0eebab8f29ada947434640e4537501151779c9
Niklas Franzke <franzke.niklas@gmail.com>
2022-03-02 12:40:26 +0100

    Scraping works; Seeding to be finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,56 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "open-uri"
+require "nokogiri"
+
+Course.destroy_all
+puts "Destroying all courses"
+User.destroy_all
+puts "Destroying all users"
+
+# Creating an array of with URL of subtopics of 5th class
+url = "https://www.khanacademy.org/math/cc-fifth-grade-math"
+html = URI.open(url)
+doc = Nokogiri::HTML(html)
+
+subctopic_links = doc.search("._1jkfus0z ._dwmetq")
+
+hrefs = []
+subctopic_links.each do |link|
+  hrefs << link["href"]
+end
+# puts hrefs
+# puts hrefs.count
+
+#Going into the URL of subtopic and retrieving all data for courses and subtopics 5th class
+hrefs.each do |href|
+  url_subtopic = "https://www.khanacademy.org#{href}"
+  html_subtopic = URI.open(url_subtopic)
+  doc_subtopic = Nokogiri::HTML(html_subtopic)
+  puts doc_subtopic
+
+  # Retrieving "topic" in the table "courses"
+    course_topic_links = doc_subtopic.search("._1eqoe4n8")
+
+    course_topics = []
+    course_topic_links.each do |course_topic|
+      course_topics << course_topic.text.strip
+    end
+
+  #Retrieving "description" in the table "subtopics"
+    course_subtopic_links = doc_subtopic.search("._lqicet ._dwmetq")
+
+    subtopic_description = []
+    course_subtopic_links.each do |course_topic|
+      subtopic_description << course_topic.text.strip
+    end
+end
+
+# Creating 5th grade math courses
+
+niklas = User.create!(name: "Niklas", description: "Loves Maths", age: 27, school_year: 12, years_of_study: 12,
+                      learning_type: "visual", interest: "CrossFit", user_type: "teacher")
+Course.create!(school_year: 5, subject: "Maths", topic: "placeholder", performance_level: "placeholder",
+               user: niklas)
--- db/seeds_scraping.rb
+++ db/seeds_scraping.rb
@@ -1,49 +1,12 @@
 require "open-uri"
 require "nokogiri"
 
-# Creating an array of courses 5th class
-url = "https://www.khanacademy.org/math/cc-fifth-grade-math"
-html = URI.open(url)
-doc = Nokogiri::HTML(html)
-
-elements = doc.search("._dwmetq ._k50sd54")
-
-courses = []
-elements.each do |element|
-  courses << element.text.strip
-end
-puts courses
-puts courses.count
-
-# Creating an array of subtopics 5th class
-url = "https://www.khanacademy.org/math/cc-fifth-grade-math"
-html = URI.open(url)
-doc = Nokogiri::HTML(html)
-
-links = doc.search("._12yy6f6l ._dwmetq")
-puts links
-
-hrefs = []
-links.each do |link|
-  hrefs << link["href"]
-end
-puts hrefs
-puts hrefs.count
-
-#code completey wrong; just logic --> DO NOT REASSIGN!!!
-hrefs.each do |href|
-  url_2 = "https://www.khanacademy.org#{href}"
-  html_2 = URI.open(url_2)
-  doc_2 = Nokogiri::HTML(html_2)
-end
-
-
-# # Creating an array of courses
-# url = "https://www.khanacademy.org/math"
+# # Creating an array of courses 5th class
+# url = "https://www.khanacademy.org/math/cc-fifth-grade-math"
 # html = URI.open(url)
 # doc = Nokogiri::HTML(html)
 
-# elements = doc.search("._158q6at ._dwmetq")
+# elements = doc.search("._dwmetq ._k50sd54")
 
 # courses = []
 # elements.each do |element|
@@ -52,20 +15,48 @@ end
 # puts courses
 # puts courses.count
 
-# #Getting the image of the beasts
-# def scraping_img(beasts)
-#   begin
-#     beast = beasts.sample.tr_s(" ", "_")
-#     url = "https://harrypotter.fandom.com/wiki/#{beast}"
+# Creating an array of with URL of subtopics of 5th class
+url = "https://www.khanacademy.org/math/cc-fifth-grade-math"
+html = URI.open(url)
+doc = Nokogiri::HTML(html)
 
-#     html = URI.open(url)
-#     doc = Nokogiri::HTML(html)
+subctopic_links = doc.search("._1jkfus0z ._dwmetq")
 
-#     element = doc.search("aside figure a")
-#     href =  element.attribute("href").value
-#     puts href
-#   rescue => exception
-#     href = "https://s7g8.scene7.com/is/image/BradfordUK/0302984001_alt1?wid=700&hei=700&fmt=pjpeg&qlt=85,0"
-#     puts href
-#   end
-# end
+hrefs = []
+subctopic_links.each do |link|
+  hrefs << link["href"]
+end
+# puts hrefs
+# puts hrefs.count
+
+#Going into the URL of subtopic and retrieving all data for courses and subtopics 5th class
+hrefs.each do |href|
+  url_subtopic = "https://www.khanacademy.org#{href}"
+  html_subtopic = URI.open(url_subtopic)
+  doc_subtopic = Nokogiri::HTML(html_subtopic)
+  puts doc_subtopic
+
+  # Retrieving "topic" in the table "courses"
+    course_topic_links = doc_subtopic.search("._1eqoe4n8")
+
+    course_topics = []
+    course_topic_links.each do |course_topic|
+      course_topics << course_topic.text.strip
+    end
+
+  # Retrieving "performance_level" in the table "courses"
+    # performance_level_links = doc_subtopic.search("._duavrzj ._1ys4rws4")
+
+    # performance_levels = []
+    # performance_level_links.each do |performance_level|
+    #   performance_levels << performance_level.text.strip
+    # end
+
+  #Retrieving "description" in the table "subtopics"
+    course_subtopic_links = doc_subtopic.search("._lqicet ._dwmetq")
+
+    subtopic_description = []
+    course_subtopic_links.each do |course_topic|
+      subtopic_description << course_topic.text.strip
+    end
+end
