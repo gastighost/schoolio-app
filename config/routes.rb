5005f02cc4f64eb9d7511e9790163092ddb0afc9
Gaston Roxas <gacaro51597@gmail.com>
2022-03-02 09:32:37 +0100

    fix review and bookmark models

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :courses do
     resources :subtopics, only: [:show, :new, :create, :edit, :update ] do
-      resources :lessons, only: [:show, :new, :create, :edit, :update]
+      resources :lessons, only: [:show, :new, :create, :edit, :update] do
+        resources :bookings, only: :create
+      end
     end
   end
   resources :subtopics, only: :destroy
   resources :lessons, only: :destroy
+  resources :bookings, only: [:index, :show, :destroy]
 end
