a755f75043a43165fa6360b6bcc7938444e597b8
Gaston Roxas <gacaro51597@gmail.com>
2022-03-03 22:57:37 +0100

    finished chat functionality for each booking

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -3,4 +3,10 @@ class ChatroomsController < ApplicationController
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
   end
+
+  def create
+    @chatroom = Chatroom.new(booking_id: params[:booking_id], name: current_user.name)
+    @chatroom.save
+    redirect_to chatroom_path(@chatroom)
+  end
 end
