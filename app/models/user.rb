378e4f782536596c3f47e03d950821afd9411d84
Gaston Roxas <gacaro51597@gmail.com>
2022-03-03 11:52:26 +0100

    added some validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ApplicationRecord
   has_many :bookings, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, :description, :age, :school_year, :years_of_study,
+            :learning_type, :interest, :user_type, presence: true
+
+  validates :school_year, inclusion: { in: 1..13,
+                                       message: "Please pick a year level from 1 to 13" }
+
+  validates :learning_type, inclusion: { in: %w[visual auditory reading writing kinesthetic] }
+
+  validates :user_type, inclusion: { in: %w[teacher student] }
 end
