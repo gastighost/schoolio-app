0769212e40680dd10f657016e27b34510d128a88
Gaston Roxas <gacaro51597@gmail.com>
2022-03-07 11:12:09 +0100

    add student name to teacher booking dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :description, :age, :school_year, :years_of_study, :learning_type, :interest, :user_type])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :description, :age, :school_year, :years_of_study, :learning_type, :interest, :user_type, :performance_level])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :description, :age, :school_year, :years_of_study, :learning_type, :interest, :user_type])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :description, :age, :school_year, :years_of_study, :learning_type, :interest, :user_type, :performance_level])
   end
 end
