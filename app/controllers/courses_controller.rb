99a2a2922010e343567084b4aed7dbd68aae729b
Gaston Roxas <78007783+gastighost@users.noreply.github.com>
2022-03-08 23:06:50 +0100

    Update courses_controller.rb

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -8,7 +8,7 @@ class CoursesController < ApplicationController
     end
     respond_to do |format|
       format.html # Follow regular flow of Rails
-      format.text { render partial: 'list.html', locals: { movies: @movies } }
+      format.text { render partial: 'list.html', locals: { courses: @courses } }
     end
   end
 
