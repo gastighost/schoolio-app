2810825b984ab858183b7497d0926fc030b588ed
Gaston Roxas <gacaro51597@gmail.com>
2022-03-08 14:36:55 +0100

    finished booking view logic for teacher and student

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,24 +1,25 @@
 class BookingsController < ApplicationController
   def index
-    @title = "Your Bookings"
-    @bookings = Booking.all
     @type = ""
     if current_user.user_type == "teacher"
+      @title = "Your Courses"
       @type = "student"
+      @courses = Course.where(user_id: current_user.id)
     elsif current_user.user_type == "student"
+      @title = "Your Bookings"
       @type = "teacher"
-    end
+      @bookings = Booking.where(user_id: current_user.id)
+      @data_keys1 = @bookings.map { |booking| booking.lesson.date.strftime("%d.%m.%Y") }
+      @data_values1 = @bookings.map { |booking| booking.performance_level.nil? ? booking.performance_level = 0 : booking.performance_level }
 
-    @data_keys1 = @bookings.map { |booking| booking.lesson.date.strftime("%d.%m.%Y") }
-    @data_values1 = @bookings.map { |booking| booking.performance_level.nil? ? booking.performance_level = 0 : booking.performance_level }
-
-    @complete = []
-    @data_keys1.each_with_index do |_key, index|
-      @complete.push([@data_keys1[index], @data_values1[index]])
+      @complete = []
+      @data_keys1.each_with_index do |_key, index|
+        @complete.push([@data_keys1[index], @data_values1[index]])
+      end
+      @completed = @complete.sort { |a, b| a[0] <=> b[0] }
+      @data_keys = @completed.map { |data| data[0] }
+      @data_values = @completed.map { |data| data[1] }
     end
-    @completed = @complete.sort { |a, b| a[0] <=> b[0] }
-    @data_keys = @completed.map { |data| data[0] }
-    @data_values = @completed.map { |data| data[1] }
   end
 
   def show
